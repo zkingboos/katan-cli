8ba62b3c9eb86509251bfd620817694b70048893
Natan Vieira <natanvnascimento@gmail.com>
2021-08-01 20:18:46 -0300

    chore: add npm publication

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,14 @@
+import io.github.gciatto.kt.node.*
+
 plugins {
+    id("com.github.johnrengelman.shadow") version "6.0.0"
+    id("io.github.gciatto.kt-npm-publish") version "0.3.9"
     kotlin("multiplatform") version Libs.kotlinVersion
     kotlin("plugin.serialization") version Libs.kotlinVersion
-    id("com.github.johnrengelman.shadow") version "6.0.0"
     application
 }
+
+val PROJECT = "katan-cli"
 
 group = "gg.katan.cli"
 
@@ -24,13 +29,17 @@ fun org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTargetWithHostTests.entry
     }
 }
 
+java {
+    disableAutoTargetJvm()
+}
+
 kotlin {
     jvm()
     macosX64 { entryPoint() }
     mingwX64 { entryPoint() }
     linuxX64 { entryPoint() }
 
-    js {
+    js(LEGACY) {
         nodejs()
         binaries.executable()
     }
@@ -45,5 +54,29 @@ kotlin {
                 implementation(Libs.KTX.Coroutines.core)
             }
         }
+    }
+}
+
+npmPublishing {
+    val organization = "katan.gg"
+
+    token.set(System.getenv("NPM_AUTH_TOKEN"))
+    liftPackageJson {
+        main = PROJECT
+        homepage = "https://github.com/KatanPanel/katan-cli"
+        bugs = Bugs("https://github.com/KatanPanel/katan-cli/issues")
+        license = "MIT"
+        bins = mutableMapOf(PROJECT to "./$PROJECT")
+        keywords = mutableListOf("katan", "cli", "kotlin", "multiplatform")
+        files = mutableListOf("bin/$PROJECT")
+        name = "@$organization/$name"
+        dependencies = dependencies?.mapKeys { (key, _) ->
+            if (name!! in key) "@$organization/$key" else key
+        }?.toMutableMap()
+    }
+
+    liftJsSources { _, _, line ->
+        line.replace("'$PROJECT", "'@$organization/$PROJECT")
+            .replace("\"$PROJECT", "\"@$organization/$PROJECT")
     }
 }

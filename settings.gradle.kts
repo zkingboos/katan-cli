1e2fdb1401970e664763458c717bea55db2c38b7
Natan Vieira <natanvnascimento@gmail.com>
2021-09-07 19:02:56 -0300

    build(gradle/settings): add buildscan

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,4 +5,20 @@ pluginManagement {
     }
 }
 
+plugins {
+    id("com.gradle.enterprise") version "3.6.4"
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        publishAlways()
+        buildScanPublished {
+            file("buildscan.log").appendText("${java.util.Date()} - $buildScanUri\n")
+        }
+    }
+
+}
+
 rootProject.name = "katan-cli"

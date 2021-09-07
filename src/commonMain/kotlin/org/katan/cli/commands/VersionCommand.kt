59ab034083881e364c0d3595692744bc89390b23
Natan Vieira <natanvnascimento@gmail.com>
2021-09-07 19:06:55 -0300

    refactor: remove platform print from `katan version` command

--- src/commonMain/kotlin/org/katan/cli/commands/VersionCommand.kt
+++ src/commonMain/kotlin/org/katan/cli/commands/VersionCommand.kt
@@ -1,18 +1,16 @@
 package org.katan.cli.commands
 
-import com.github.ajalt.clikt.core.CliktCommand
-import com.github.ajalt.mordant.rendering.TextColors
-import org.katan.cli.KATAN_VERSION
-import org.katan.cli.PRIMARY_COLOR
-import org.katan.cli.platform
-import org.katan.cli.terminal
+import com.github.ajalt.clikt.core.*
+import com.github.ajalt.mordant.rendering.*
+import org.katan.cli.*
 
 class VersionCommand : CliktCommand(
     name = "version",
+    help = "Show the Katan version information"
 ) {
 
     override fun run() {
-        terminal.println(TextColors.rgb(PRIMARY_COLOR)("Katan-CLI v$KATAN_VERSION @ $platform"))
+        terminal.println(TextColors.rgb(PRIMARY_COLOR)("Katan-CLI v$VERSION"))
     }
 
 }

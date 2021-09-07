1d57f8a60882f5299e49157f08abba751d089b4b
Natan Vieira <natanvnascimento@gmail.com>
2021-09-07 19:06:15 -0300

    build(libs): add `katan-sdk` and downgrade Kotlin version to `1.5.10`

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -1,14 +1,15 @@
 object Libs {
 
-    const val kotlinVersion = "1.5.20"
+    const val kotlinVersion = "1.5.10"
 
     const val clikt = "com.github.ajalt.clikt:clikt:3.2.0"
     const val mordant = "com.github.ajalt.mordant:mordant:2.0.0-beta2"
+    const val katanSdk = "org.katan:katan-sdk:0.0.1"
 
     object KTX {
         object Coroutines {
             private const val prefix = "org.jetbrains.kotlinx:kotlinx-coroutines"
-            private const val version = "1.5.1"
+            const val version = "1.5.0-native-mt"
 
             const val core = "$prefix-core:$version"
         }
